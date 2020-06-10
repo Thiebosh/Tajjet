b0c9be724d5cbd36c3dafd1d0448efe0e3c9353f
Thibaut Juzeau <thibaut.juzeau@isen.yncrea.fr>
2020-06-10 11:57:31 +0200

    remaniement arborescence python + description

---
+++ core/main_function.py
@@ -0,0 +1,8 @@
+import requests
+
+#response = requests.get('https://webnext.fr/templates/webnext_exclusive/views/includes/epg_cache/programme-tv-rss_07-06-2020.xml')
+#print(response.content)
+
+print("success")
+
+#put here switch or something who call module(s) depending of arguments or json (modules names)
