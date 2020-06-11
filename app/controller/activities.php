5cbd8b91907575aa47ed741394dd9bb824f50c98
Thibaut Juzeau <thibaut.juzeau@isen.yncrea.fr>
2020-06-11 17:03:10 +0200

    end module page variables

--- app/controller/activities.php
+++ app/controller/activities.php
@@ -6,7 +6,71 @@
 //2. appels bdd
 //load bdd functions : require("../model/manager/*needed*.php");
 //call managers functions (load data here)
+//if (weather->isgood()) {
+$pageFill['indoor'] = array(
+                            array(
+                                "channel" => 1,
+                                "program" => array(
+                                                array(
+                                                        "begin" => 15-50,
+                                                        "end" => 16-20,
+                                                        "title" => "petits meurtes d'agatha christie",
+                                                        "synopsis" => "lui l'est venu, lui l'est mourru, lui l'est coupâââââble",
+                                                        "genre" => array("genre1", "genre4")
+                                                    ),
+                                                    
+                                                array(
+                                                    "begin" => 16-30,
+                                                    "end" => 18-10,
+                                                    "title" => "eragon",
+                                                    "synopsis" => "dragons, épées, magie...",
+                                                    "genre" => array("genre2")
+                                                    )
+                                                )
+                            ),
+                            array(
+                                "channel" => 2,
+                                "program" => array(
+                                                array(
+                                                        "begin" => 12-30,
+                                                        "end" => 13-00,
+                                                        "title" => "midi les zouzou",
+                                                        "synopsis" => "c'est vieux, je sais",
+                                                        "genre" => array("genre3", "genre4")
+                                                    ),
+                                                    
+                                                array(
+                                                    "begin" => 16-00,
+                                                    "end" => 19-40,
+                                                    "title" => "eyeshield 21",
+                                                    "synopsis" => "pareil, c'est pas tout récent",
+                                                    "genre" => array("genre1", "genre3", "genre4")
+                                                    )
+                                                )
+
+                            )
+                        );
+//}
+//else {
+$pageFill['outdoor'] = array(
+                            array(
+                                "label" => "musée",
+                                "distance" => 10,
+                                "category" => "cat1",
+                            ),
+                            array(
+                                "label" => "parc",
+                                "distance" => 2,
+                                "category" => "cat3",
+                            ),
+                            array(
+                                "label" => "restau",
+                                "distance" => 2.4,
+                                "category" => "cat2",
+                            )
+                        );
+//}
 
 
 //3. transforme donnees (post traitement)
-//tranformations goes here
+//durée
