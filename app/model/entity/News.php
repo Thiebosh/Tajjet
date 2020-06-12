b209205e85b7fb2966ccae932fbb49890bcf0f8c
Thibaut Juzeau <thibaut.juzeau@isen.yncrea.fr>
2020-06-12 10:48:54 +0200

    begin DAO : entity

---
+++ app/model/entity/News.php
@@ -0,0 +1,59 @@
+<?php
+require_once(__DIR__."/../abstract/Entity.php");
+
+require_once("Article.php");
+require_once("Theme.php");
+
+class News extends Entity {
+    //attributes
+    private $_summary;
+    private $_Theme;//instance de Theme
+    //private $_Articles;//tableau d'instances d'Articles
+
+
+    //methods
+
+    //constructor & destructor
+    public function __construct(array $data) {
+        parent::__construct($data);
+    }
+
+    public function __destruct() {
+        parent::__destruct();
+    }
+
+
+    //getters
+    public function getSummary() {
+        return this->_summary;
+    }
+
+    public function getTheme() {
+        return this->_Theme;
+    }
+/*
+    public function getArticles() {
+        return this->_Articles;
+    }
+    */
+
+
+    //setters
+    public function setSummary($summary) {
+        if (is_string($summary)) this->_summary = $summary;
+    }
+
+    public function setTheme($theme) {
+        if ($theme instanceof Theme) this->_Theme = $theme;
+    }
+
+    /*
+    public function setArticles($articles) {
+        foreach ($articles as $article) addArticle($article);
+    }
+
+    public function addArticle($article) {
+        if ($article instanceof Article) this->_Articles[] = $article;
+    }
+    */
+}
---
+++ app/model/entity/Theme.php
@@ -0,0 +1,31 @@
+<?php
+require_once(__DIR__."/../abstract/Entity.php");
+
+class News extends Entity {
+    //attributes
+    private $_label;
+
+
+    //methods
+
+    //constructor & destructor
+    public function __construct(array $data) {
+        parent::__construct($data);
+    }
+
+    public function __destruct() {
+        parent::__destruct();
+    }
+
+
+    //getters
+    public function getLabel() {
+        return this->_URL;
+    }
+
+
+    //setters
+    public function setLabel($label) {
+        if (is_string($label)) this->_label = $label;
+    }
+}
