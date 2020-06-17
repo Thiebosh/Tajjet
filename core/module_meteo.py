6d0fd2d2199d89b959970518121ee7f372bc432c
FiftyTwoWays <tristanmlct@gmail.com>
2020-06-17 18:10:46 +0200

    Fix module météo

--- core/module_meteo.py
+++ core/module_meteo.py
@@ -25,47 +25,43 @@ data = res.json()
 
 #pprint(data) #affichage de toutes les données sur chaque cycle
 
-for i in range(0, len(data['list'])):  
-
-      Forecast = data['list'][i]['dt_txt']
-      year = ""
-      for i in range(0,4):
+for j in range(0, len(data['list'])): 
+    Forecast = data['list'][j]['dt_txt']
+    year = ""
+    for i in range(0,4):
         year += Forecast[i]
 
-      month = ""
-      for i in range(5, 7):
+    month = ""
+    for i in range(5, 7):
         month += Forecast[i]
 
-      day = ""
-      for i in range(8, 10):
-        day += Forecast[i]
+    day = ""
+    for i in range(8, 10):
+        day += Forecast[i]    
 
-      hour = ""
-      for i in range(11, 16):
-            hour += Forecast[i]
+    hour = ""
+    for i in range(11, 16):
+        hour += Forecast[i]
 
-      Forecast = day +"-"+month+"-"+year+ " "+ hour
+    Forecast = day +"-"+month+"-"+year+ " "+ hour
+    
+    label = data['list'][j]['weather'][0]['main']
+    MinTemp = data['list'][j]['main']['temp_min']
+    MaxTemp = data['list'][j]['main']['temp_max']
+    FeltTemp = data['list'][j]['main']['feels_like']
+    Humidity = data['list'][j]['main']['humidity']
+    Pressure = data['list'][j]['main']['pressure']
 
-      label = data['list'][i]['weather'][0]['main']
-      MinTemp = data['list'][i]['main']['temp_min']
-      MaxTemp = data['list'][i]['main']['temp_max']
-      FeltTemp = data['list'][i]['main']['feels_like']
-      Humidity = data['list'][i]['main']['humidity']
-      Pressure = data['list'][i]['main']['pressure']
+    print(Forecast, MinTemp, MaxTemp, FeltTemp, Humidity, Pressure)
 
-      print(Forecast, MinTemp, MaxTemp, FeltTemp, Humidity, Pressure)
-            
-            
+    mycursor = mydb.cursor(buffered=True)
 
-
-      mycursor = mydb.cursor(buffered=True)
-      
-      sql = "SELECT id_town FROM town WHERE label=%s"
-      adr = (city, )
-      mycursor.execute(sql, adr)
+    sql = "SELECT id_town FROM town WHERE label=%s"
+    adr = (city, )
+    mycursor.execute(sql, adr)
 
 
-      if(mycursor.rowcount == 0):
+    if(mycursor.rowcount == 0):
         sql = "INSERT INTO town (label) VALUES (%s)"
         adr = (city, )
         mycursor.execute(sql, adr)
@@ -75,18 +71,17 @@ for i in range(0, len(data['list'])):
         adr = (city, )
         mycursor.execute(sql, adr)
 
-      myresult = mycursor.fetchall()
-      id_town = myresult[0][0]
-      print(myresult[0][0])
+    myresult = mycursor.fetchall()
+    id_town = myresult[0][0]
 
-      #verif sky
+    #verif sky
 
-      sql = "SELECT id_sky FROM sky WHERE label=%s"
-      adr = (label, )
-      mycursor.execute(sql, adr)
+    sql = "SELECT id_sky FROM sky WHERE label=%s"
+    adr = (label, )
+    mycursor.execute(sql, adr)
 
 
-      if(mycursor.rowcount == 0):
+    if(mycursor.rowcount == 0):
         sql = "INSERT INTO sky (label) VALUES (%s)"
         adr = (label, )
         mycursor.execute(sql, adr)
@@ -96,17 +91,16 @@ for i in range(0, len(data['list'])):
         adr = (label, )
         mycursor.execute(sql, adr)
 
-      myresult = mycursor.fetchall()
-      id_sky = myresult[0][0]
-      print(myresult[0][0])
+    myresult = mycursor.fetchall()
+    id_sky = myresult[0][0]
 
 
 
-      sql = "INSERT INTO weather (Forecast, MinTemp, MaxTemp, FeltTemp, Humidity, Pressure, id_sky, id_town) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-      val = (Forecast, MinTemp, MaxTemp, FeltTemp, Humidity, Pressure, id_sky, id_town)
-      mycursor.execute(sql, val)
+    sql = "INSERT INTO weather (Forecast, MinTemp, MaxTemp, FeltTemp, Humidity, Pressure, id_sky, id_town) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (Forecast, MinTemp, MaxTemp, FeltTemp, Humidity, Pressure, id_sky, id_town)
+    mycursor.execute(sql, val)
 
-      mydb.commit()
+    mydb.commit()
 
 
 
