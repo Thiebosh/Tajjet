f40189428d6886b4c483d6adb850d6d668f47e25
Thibaut Juzeau <thibaut.juzeau@isen.yncrea.fr>
2020-06-19 12:07:25 +0200

    condition home

--- app/controller/home.php
+++ app/controller/home.php
@@ -1,8 +1,12 @@
 <?php
 
-$grid = array(  array(  array("link" => "sport",        "display" => "Sport"),
-                        array("link" => "health",       "display" => "Santé"),
-                        array("link" => "news",         "display" => "News")),
-                array(  array("link" => "weather",      "display" => "Météo"),
-                        array("link" => "activities",   "display" => "Activités"),
-                        array("link" => "cooking",      "display" => "Recettes")));
+$healthCond = $_SESSION['user']->getHeight() != null ? true : false;
+$weatherCond = $_SESSION['user']->getTown()->getId() != null ? true : false;
+
+$grid = array(  array(  array("link" => "sport",        "display" => "Sport",       "active" => true),
+                        array("link" => "health",       "display" => "Santé",       "active" => $healthCond,    "missing" => "taille"),
+                        array("link" => "news",         "display" => "News",        "active" => true)),
+
+                array(  array("link" => "weather",      "display" => "Météo",       "active" => $weatherCond,   "missing" => "ville"),
+                        array("link" => "activities",   "display" => "Activités",   "active" => true),
+                        array("link" => "cooking",      "display" => "Recettes",    "active" => true)));
