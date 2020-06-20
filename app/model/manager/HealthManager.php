b7b1a02b08a8f7b0005fb6e5488bd18100c7b13c
Jean <jeandepamp@gmail.com>
2020-06-21 01:48:31 +0200

    bug fixes health

--- app/model/manager/HealthManager.php
+++ app/model/manager/HealthManager.php
@@ -3,6 +3,7 @@ require_once(__DIR__."/../abstract/Manager.php");
 require_once(__DIR__."/../entity/Health.php");
 
 class HealthManager extends Manager {//pattern CRUD : create, read, update, delete + methodes pratiques
+   
     public function createTodayRecord($health) {
         $query = 'INSERT INTO Health(RecordDate, Weight, Calories, Sleep, ID_user)
                     VALUES(NOW(), :weight, :calories, :sleep, :id)';
@@ -12,33 +13,25 @@ class HealthManager extends Manager {//pattern CRUD : create, read, update, dele
                         'weight'    => ($health->getWeight()    != null) ? $health->getWeight()     : PDO::PARAM_NULL);
 
         $request = parent::prepareAndExecute($query, $table);
-
-
-        $query = 'SELECT *
-                    FROM Health 
-                    WHERE RecordDate = NOW()';
         
-        $request = parent::prepareAndExecute($query);
-
-        $result = $request->fetchAll(PDO::FETCH_ASSOC);//fetchAll => close cursor implicite
-
-        return new Health($result[0]);//gagne un id
+        return $this->readTodayRecord($health->getIdUser());
     }
 
+    
 
-    public function readTodayRecord($userId) {
+    public function readTodayRecord($idUser) {
         $query = "SELECT * 
                     FROM Health 
                     WHERE RecordDate = NOW()
                     AND ID_user = :id";
         $table = array('id' => $idUser);
-
         $request = parent::prepareAndExecute($query, $table);
 
         $result = $request->fetchAll(PDO::FETCH_ASSOC);//fetchAll => close cursor implicite
-
+        
         return (count($result) != 0) ? new Health($result[0]) : false;
     }
+    
 
 
     public function readLast7Days($idUser) {
@@ -58,6 +51,7 @@ class HealthManager extends Manager {//pattern CRUD : create, read, update, dele
 
         return (count($result) != 0) ? $result : false;
     }
+    
 
 
     public function updateTodayRecord($health) {
