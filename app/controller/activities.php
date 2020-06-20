9489a513f5ca2695c812ca318ba22ab6fd131b87
Thibaut Juzeau <thibaut.juzeau@isen.yncrea.fr>
2020-06-21 00:27:38 +0200

    correctif tv captation

--- app/controller/activities.php
+++ app/controller/activities.php
@@ -1,7 +1,14 @@
 <?php
 require_once(__DIR__."/../model/manager/TVprogramManager.php");
 
-foreach ((new TVprogramManager)->readAll(false) as $line) $activitiesListNotStarted[] = $line->objectToJson();
-foreach ((new TVprogramManager)->readAll(true) as $line) $activitiesListStarted[] = $line->objectToJson();
+$data = (new TVprogramManager)->readAll(false);
+if ($data != false) foreach ($data as $line) $activitiesListNotStarted[] = $line->objectToJson();
 
-$data = json_encode(array("notStarted" => $activitiesListNotStarted, "started" => $activitiesListStarted));
+$data = (new TVprogramManager)->readAll(true);
+if ($data != false) foreach ($data as $line) $activitiesListStarted[] = $line->objectToJson();
+
+unset($data);
+if (isset($activitiesListNotStarted)) $data["notStarted"] = $activitiesListNotStarted;
+if (isset($activitiesListStarted)) $data["started"] = $activitiesListStarted;
+
+$data = json_encode($data);
